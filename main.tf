b7252fe877b389cb64900809cba824d750ea3cdb
marinaivanova2025 <marina.ivanova@hashicorp.com>
2025-10-20 11:57:28 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,12 +11,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "my-unique-test-bucket-12345" 
-  acl    = "private"
+resource "aws_api_gateway_rest_api" "test_api" {
+  name        = "tf-test-api"
+  description = "Test API Gateway to reproduce outage"
+}
 
-  tags = {
-    Name        = "TestBucket"
-    Environment = "Test"
-  }
+resource "aws_api_gateway_resource" "test_resource" {
+  rest_api_id = aws_api_gateway_rest_api.test_api.id
+  parent_id   = aws_api_gateway_rest_api.test_api.root_resource_id
+  path_part   = "test"
+}
+
+resource "aws_api_gateway_method" "test_method" {
+  rest_api_id   = aws_api_gateway_rest_api.test_api.id
+  resource_id   = aws_api_gateway_resource.test_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "test_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.test_api.id
+  resource_id             = aws_api_gateway_resource.test_resource.id
+  http_method             = aws_api_gateway_method.test_method.http_method
+  integration_http_method = "GET"
+  type                    = "MOCK"
+}
+
+resource "aws_api_gateway_deployment" "test_deployment" {
+  depends_on = [aws_api_gateway_integration.test_integration]
+  rest_api_id = aws_api_gateway_rest_api.test_api.id
+  stage_name  = "dev"
 }
